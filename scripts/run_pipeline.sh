c92786ea98bbd4e65edbb049adac9f1ed9f15a5f
dylanpilz <dpilz@ucsd.edu>
2023-12-13 09:25:44 -0500

    run just 1 batch

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -3,20 +3,18 @@ set -x
 
 BATCH_SIZE=50
 
-for i in {1..10}
-do
-    nextflow run main.nf \
-        --input data/samples_to_run.csv \
-        --num_samples $BATCH_SIZE \
-        -profile docker \
-        -entry fetch_sra &
-    BACK_PID=$!
-    wait $BACK_PID
-    rm -rf work
 
-    sed -i '2,51d' data/samples_to_run.csv
-done
 
-python scripts/aggregate_demix.py .
+nextflow run main.nf \
+    --input data/samples_to_run.csv \
+    --num_samples $BATCH_SIZE \
+    -profile docker \
+    -entry fetch_sra &
 BACK_PID=$!
 wait $BACK_PID
+rm -rf work
+
+sed -i '2,51d' data/samples_to_run.csv
+
+
+python scripts/aggregate_demix.py . || true
